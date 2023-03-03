c90249a21293ec6c4c6e670af9075b5105b38f25
Laura DAddese <laura.daddese@heig-vd.ch>
2023-03-03 13:54:40 +0100

    02-intro-d3 début de l'exercie

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -2,3 +2,51 @@ import {select} from "d3-selection";
 
 // C'est ici que vous allez écrire les premières lignes avec d3.js!
 
+
+let svg1 = select("body")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+    .style('background-color', "lightgrey")
+
+let circle1 = svg1.append("circle")
+.join(enter => enter
+	.attr("cx", 50)
+    .attr("cy", 50)
+    .attr("r", 40)
+    .style("fill", "purple"),
+	update => update
+	.attr("cx", 100))
+    .attr("cy", 50)
+    .attr("r", 40)
+    .style("fill", "purple");
+	
+
+	let circle2 = svg1.append("circle")
+    .join(enter => enter
+		.attr("cx", 150)
+		.attr("cy", 150)
+		.attr("r", 40)
+		.style("fill", "purple"),
+		update => update
+		.attr("cx", 200))
+		.attr("cy", 150)
+		.attr("r", 40)
+		.style("fill", "blue");
+
+	let circle3 = svg1.append("circle")
+	.join(enter => enter
+	.attr("cx", 250)
+	
+    .attr("cy", 250)
+    .attr("r", 40)
+    .style("fill", "purple"),
+	update => update
+	.attr("cx", 250))
+    .attr("cy", 250)
+    .attr("r", 40)
+    .style("fill", "purple");
+	
+
+
+	
